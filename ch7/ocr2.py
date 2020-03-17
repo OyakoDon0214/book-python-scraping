784bbd673455af8290ce39695a9bab4bdc2bfb64
OyakoDon0214 <metbyroot@gmail.com>
2020-03-17 15:32:12 +0900

    うまく行った

--- ch7/ocr2.py
+++ ch7/ocr2.py
@@ -1,9 +1,11 @@
+#%%
 import sys
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
 # 画像の読み込み
-im = cv2.imread('numbers100.png')
+im = cv2.imread('IMG_2BCE1EB797B2-1.JPG')
+image=cv2.imread('IMG_2BCE1EB797B2-1.JPG')
 # グレイスケールに変換しぼかした上で二値化する 
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -14,11 +16,30 @@ contours = cv2.findContours(
     thresh,      
     cv2.RETR_EXTERNAL, 
     cv2.CHAIN_APPROX_SIMPLE)[0]
-
 # 抽出した領域を繰り返し処理する 
+n=0
+n_plot=[]
+y_plot=[]
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt) # --- (※5)
     if h < 10: continue # 小さすぎるのは飛ばす
     cv2.rectangle(im, (x, y), (x+w, y+h), (0,0,255), 2)
-cv2.imwrite('numbers100-cnt2.png', im)
+    y_plot.append(y)
+    n_plot.append(n)
+    n+=1
+#plt.plot(n_plot,y_plot)
+#plt.show()
+cv2.imwrite('numbers100-cnt2.png',im)
 
+#%%
+trimmed_image=image[765:810,]
+cv2.imwrite("trimmed_image.png", trimmed_image)
+
+# %%
+print(trimmed_image.shape)
+
+# %%
+im.shape
+
+
+# %%
